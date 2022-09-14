5280ee54c2bfea4305a5575685aa48aa94ddb9f6
Hiferli <joshi.ishaan.2001@gmail.com>
2022-09-14 11:32:53 +0530

    Information from elements

--- main.py
+++ main.py
@@ -11,5 +11,37 @@ webHTML = website_content.content
 # print(webHTML)
 
 # Parsing the HTML
-webParse = BeautifulSoup(webHTML , 'html.parser')
-print(webParse.prettify())
+soup = BeautifulSoup(webHTML , 'html.parser')
+# print(soup.prettify())
+
+# HTML Tree Traversal
+
+# Getting the Title
+webTitle = soup.find("title")
+# print(webTitle)
+
+# Getting any specific tags
+webParas = soup.find_all('a');
+# print(webParasClass)
+
+# Getting tags with specific class
+webClassElements = soup.find_all('li' , class_ = "mx-2")
+# print(webClassElements);
+
+# Getting text from tags
+# print(soup.find('p').get_text());
+
+# Getting all links in the page
+anchor = soup.find_all('a')
+
+all_links = set();
+for links in anchor:
+    link = links.get('href')
+    # print(link)
+    if(link != '#'):
+        if(link[0] == '/'):
+            all_links.add(websiteURL + link);
+        elif(link[0] == 'h'):
+            all_links.add(link);
+            
+print(all_links)
